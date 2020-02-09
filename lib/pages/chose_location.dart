1751bfe1e280b91f66e8ce9466b13a68afc9bd3b
Melrok <martin.blascak86@gmail.com>
2020-02-09 17:48:35 +0100

    pridané vlajky a práca zo šablónami pre card()

--- lib/pages/chose_location.dart
+++ lib/pages/chose_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:multipageapp/services/world_time.dart';
 
 class ChooseLocation extends StatefulWidget {
   @override
@@ -6,18 +7,55 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+  List<WorldTime> locations = [
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTime(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+    WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amberAccent,
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        backgroundColor: Colors.amber,
-      ),
-      body: Text('Coose Location in AafeArea',
-        style: TextStyle(
-          fontSize: 22.0,
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose a location',
+          style: TextStyle(
+            fontSize: 24.0,
+            letterSpacing: 1.2,
+          ),
         ),
+        centerTitle: true,
+        elevation: 0,
       ),
+      body: ListView.builder(
+          itemCount: locations.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.fromLTRB(5.0, 4.0, 5.0, 4.0),
+              child: Card(
+                child: ListTile(
+                  onTap:() {},
+                  title: Text(locations[index].location,
+                    style: TextStyle(
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage('assets/img/${locations[index].flag}'),
+                  ),
+                ),
+              ),
+            );
+          }
+      )
     );
   }
 }
